16eec35655536de012aafced724fd3a997e916b8
James <james.a.tracy@gmail.com>
2013-12-31 12:03:10 -0800

    Link billing navigation to coming soon page

--- app/views/partials/navigation.php
+++ app/views/partials/navigation.php
@@ -2,7 +2,7 @@
 	<ul class="nav navbar-nav">
 	  <li><a href="<?php echo $this->urlAlias("patient-visitor-services"); ?>">Patient &amp; Visitors</a></li>
 	  <li><a href="<?php echo $this->urlAlias("physician-services-directory"); ?>">Physician &amp; Services Directory</a></li>
-	  <li><a href="#">Billing &amp; Insurance</a></li>
+	  <li><a href="<?php echo $this->urlAlias("coming-soon"); ?>" title="Coming Soon!">Billing &amp; Insurance</a></li>
 	  <li><a href="<?php echo $this->urlAlias("about"); ?>">About Appling</a></li>
 	  <li><a href="<?php echo $this->urlAlias("jobs"); ?>">Job Opportunities</a></li>
 	  <li><a href="<?php echo $this->urlAlias("contact"); ?>">Contact Appling</a></li>
