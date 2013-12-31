0d0b0dec6c0da3050c89f04b62ed0049525e1eeb
James <james.a.tracy@gmail.com>
2013-12-31 12:00:53 -0800

    More portal link stuff

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -7,7 +7,7 @@
 		</div>
 		<div class="icons pull-right">
 			<a href="https://srvahcsptweb.myahcs.org" title="Hospital Patient Portal" target="_blank"><img class="hospital-portal" src="<?php echo $this->url("/assets/images/hospital-portal-icon.jpg"); ?>" width="43" height="60" alt="Hospital Patient Portal"/></a>
-			<a href="<?php echo $this->urlAlias('coming-soon'); ?>" title="Clinical Patient Portal"><img class="patient-portal" src="<?php echo $this->url("/assets/images/patient-portal-icon.jpg"); ?>" width="53" height="60" alt="Clincial Patient Portal"/></a>
+			<a href="<?php echo $this->urlAlias('coming-soon'); ?>" title="Clinical Patient Portal -- Coming Soon!"><img class="patient-portal" src="<?php echo $this->url("/assets/images/patient-portal-icon.jpg"); ?>" width="53" height="60" alt="Clincial Patient Portal"/></a>
 		</div>
 	</div>
 </header>
