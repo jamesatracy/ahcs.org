03471eb2bbeb4291cfb9382ab7df451eed9ffe3e
James <james.a.tracy@gmail.com>
2013-12-31 17:47:07 -0800

    Update the header look and feel

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -1,13 +1,17 @@
 <header id="main-page-header">
 	<div class="container">
-		<div class="pull-left">
-			<a href="<?php echo $this->urlAlias("home"); ?>">
-				<img id="logo" src="<?php echo $this->url("/assets/images/app_logo_188x120.png"); ?>" width="188" height="120" alt="Appling HealthCare System"/>
-			</a>
-		</div>
-		<div class="icons pull-right">
-			<a href="https://srvahcsptweb.myahcs.org" title="Hospital Patient Portal" target="_blank"><img class="hospital-portal" src="<?php echo $this->url("/assets/images/hospital-portal-icon.jpg"); ?>" width="43" height="60" alt="Hospital Patient Portal"/></a>
-			<a href="<?php echo $this->urlAlias('coming-soon'); ?>" title="Clinical Patient Portal -- Coming Soon!"><img class="patient-portal" src="<?php echo $this->url("/assets/images/patient-portal-icon.jpg"); ?>" width="53" height="60" alt="Clincial Patient Portal"/></a>
+		<div class="header-inner">
+			<div class="pull-left">
+				<a href="<?php echo $this->urlAlias("home"); ?>">
+					<img id="logo" src="<?php echo $this->url("/assets/images/app_logo_188x120.png"); ?>" width="188" height="120" alt="Appling HealthCare System"/>
+				</a>
+			</div>
+			<div class="pull-right">
+				<div class="icons">
+					<a href="https://srvahcsptweb.myahcs.org" title="Hospital Patient Portal" target="_blank"><img class="hospital-portal" src="<?php echo $this->url("/assets/images/hospital-portal-icon.jpg"); ?>" width="43" height="60" alt="Hospital Patient Portal"/></a>
+					<a href="<?php echo $this->urlAlias('coming-soon'); ?>" title="Clinical Patient Portal -- Coming Soon!"><img class="patient-portal" src="<?php echo $this->url("/assets/images/patient-portal-icon.jpg"); ?>" width="53" height="60" alt="Clincial Patient Portal"/></a>
+				</div>
+			</div>
 		</div>
 	</div>
 </header>
