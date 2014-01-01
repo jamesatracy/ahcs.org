dd116d4584c34bcc0a3f42514334db2f75b9aadc
James <james.a.tracy@gmail.com>
2013-12-31 23:07:17 -0800

    News items.

--- app/views/partials/right-rail.php
+++ app/views/partials/right-rail.php
@@ -1,9 +1,9 @@
 <h3>Appling News</h3>
-<p><a href="#"><strong>News Feed Item 1</strong></a> <em>11/20/2013</em></p>
-<p>First line or so of the news feed will server as a summary or so.</p>
+<p><a href="#"><strong>News Feed Item 1</strong></a> <em><?php echo date("m/d/Y"); ?></em></p>
+<p>First line or so of the news feed will serve as a summary.</p>
 
-<p><a href="#"><strong>News Feed Item 2</strong></a> <em>11/20/2013</em></p>
-<p>First line or so of the news feed will server as a summary or so.</p>
+<p><a href="#"><strong>News Feed Item 2</strong></a> <em><?php echo date("m/d/Y"); ?></em></p>
+<p>First line or so of the news feed will serve as a summary.</p>
 
-<p><a href="#"><strong>News Feed Item 3</strong></a> <em>11/20/2013</em></p>
-<p>First line or so of the news feed will server as a summary or so.</p>
+<p><a href="#"><strong>News Feed Item 3</strong></a> <em><?php echo date("m/d/Y"); ?></em></p>
+<p>First line or so of the news feed will serve as a summary.</p>
