210c4e4a12af6ee74461be04a6a332d6cd36a52e
James Tracy <james.a.tracy@gmail.com>
2014-01-09 11:33:12 -0800

    Site map updates

--- app/views/site-map.php
+++ app/views/site-map.php
@@ -9,10 +9,11 @@ $this->pageHeader = "Site Map";
 <?php $this->define("content"); ?>
 <ul>
 	<li><a href="<?php echo $this->urlAlias('home'); ?>">Home</a></li>
-	<li><a href="<?php echo $this->urlAlias('patient-visitor-services'); ?>">Patient &amp; Visitor Services</a></li>
-	<li><a href="<?php echo $this->urlAlias('physicians-services-directory'); ?>">Physician &amp; Services Directory</a></li>
-	<li><a href="<?php echo $this->urlAlias('pavilion-senior-care'); ?>">Senior Care &amp; Pavilion</a></li>
 	<li><a href="<?php echo $this->urlAlias('about'); ?>">About Appling HealthCare</a></li>
+	<li><a href="<?php echo $this->urlAlias('coming-soon'); ?>">Billing &amp; Insurance</a></li>
 	<li><a href="<?php echo $this->urlAlias('jobs'); ?>">Job Opportunities</a></li>
+	<li><a href="<?php echo $this->urlAlias('patient-visitor-services'); ?>">Patient &amp; Visitor Services</a></li>
+	<li><a href="<?php echo $this->urlAlias('physician-services-directory'); ?>">Physician &amp; Services Directory</a></li>
+	<li><a href="<?php echo $this->urlAlias('pavilion-senior-care'); ?>">Senior Care &amp; Pavilion</a></li>
 </ul>
 <?php $this->end(); ?>
