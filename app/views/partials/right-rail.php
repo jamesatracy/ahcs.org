1da926d9debd7474196b53a82e1fcca6a138c4be
James Tracy <james.a.tracy@gmail.com>
2014-01-08 17:11:33 -0800

    Added static news snippets.

--- app/views/partials/right-rail.php
+++ app/views/partials/right-rail.php
@@ -1,9 +1,9 @@
 <h3>Appling News</h3>
-<p><a href="#"><strong>News Feed Item 1</strong></a> <em><?php echo date("m/d/Y"); ?></em></p>
-<p>First line or so of the news feed will serve as a summary.</p>
+<p><strong>Geriatric Psychiatric Beds Added</strong> <em>12/27/2013</em></p>
+<p>Appling HealthCare System was granted by certificate of need from the Georgia Department of Community Health, 15 additional Geriatric Psychiatric beds on December 27th, 2013.</p>
 
-<p><a href="#"><strong>News Feed Item 2</strong></a> <em><?php echo date("m/d/Y"); ?></em></p>
-<p>First line or so of the news feed will serve as a summary.</p>
+<p><strong>Appling Healthcare ISO 9001 certified</strong> <em>12/27/2013</em></p>
+<p>Appling HealthCare System is one of two hospitals in the state of Georgia that is ISO 9001 certified.</p>
 
-<p><a href="#"><strong>News Feed Item 3</strong></a> <em><?php echo date("m/d/Y"); ?></em></p>
-<p>First line or so of the news feed will serve as a summary.</p>
+<p><strong>Hometown Health Hospital of the Year!</strong></a> <em>11/14/2013</em></p>
+<p>Appling Healthcare System was awarded the 2013 hospital of the year award by Hometown Health at the fall conference at Callaway Gardens on November 14th, 2013. </p>
