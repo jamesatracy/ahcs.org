76551956f0fa73273e2d3329f6491534082255b3
James <james.a.tracy@gmail.com>
2014-01-25 15:47:55 -0800

    Fixed table headers JS

--- htdocs/assets/js/fixed-table-headers.js
+++ htdocs/assets/js/fixed-table-headers.js
@@ -1,50 +1,54 @@
-(function () {
+(function (win, doc) {
 	var elements;
 	
-	if(!document.querySelectorAll || 
-	   !window.addEventListener) {
+	if(!doc.querySelectorAll) {
 		// graceful IE degradation
 		return;
 	}
 	
 	// initialize fixed table headers
-	elements = document.querySelectorAll('.header-fixed');
+	elements = doc.querySelectorAll('.header-fixed');
 	for (var i = 0, len = elements.length; i < len; i++) {
 		initHeader(elements[i]);
 	}
 	
 	function initHeader(el) {
 		var table,
+			thead,
 			tableHeight,
 			tableOffset,
 			rect,
 			els,
-			copyEls;
+			copyEls,
+			copyRect,
+			scroll = win.addEventListener ? 'scroll' : 'onscroll',
+			addEvent = win.addEventListener || win.attachEvent;
 		
-		table = document.getElementById(el.getAttribute('data-table'));
+		table = doc.getElementById(el.getAttribute('data-table'));
 		rect = table.getBoundingClientRect();
 		
-		// clone table header and match the width
-		el.style.width = rect.width + 'px';
-		el.appendChild(
-			table.querySelector('thead').cloneNode(true)
-		);
-		
-		els = el.getElementsByTagName('th');
+		// clone the real table's thead so we can copy it over to the fixed table
+		thead = table.querySelector('thead').cloneNode(true);
+		els = thead.children[0].children; // cloned table ths
 		copyEls = table.getElementsByTagName('th');
 		
 		// copy th widths to cloned header so they line up
 		for (var j = 0, len = copyEls.length; j < len; j++) {
-			els[j].style.width = copyEls[j].getBoundingClientRect().width + 'px';
+			copyRect = copyEls[j].getBoundingClientRect();
+			els[j].style.width = (copyRect.right - copyRect.left) + 'px';
 			els[j].style.borderBottom = '0';
 		}
+		
+		// now we can update the fixed table with the cloned nodes
+		el.style.width = (rect.right - rect.left) + 'px';
+		el.appendChild(thead);
 		
 		// cache these values
 		tableHeight = rect.height;
 		tableOffset = rect.top;
 		
-		window.addEventListener('scroll', function () {
-			var offset = window.pageYOffset || document.body.scrollTop,
+		addEvent(scroll, function () {
+			var offset = win.pageYOffset || document.body.scrollTop,
 				tableBottom = tableOffset + tableHeight;
 		
 			// determine whether we should show or hide the fixed header
@@ -53,9 +57,9 @@
 			    offset <= tableBottom) {
 				el.style.display = 'table';
 			} else if (offset < tableOffset || 
-					   offset > tableBottom) {
+				offset > tableBottom) {
 				el.style.display = 'none';
 			}
 		});
 	}
-}());
+}(window, document));
