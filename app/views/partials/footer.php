963fa95fdcc7d22de95965abb3849aad5a381166
James Tracy <james.a.tracy@gmail.com>
2014-03-19 16:55:01 -0700

    A11y updates

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,35 +1,37 @@
-<section id="content-footer" class="container content">
-	<div class="row">
-		<div class="col-xs-4 first">
-			Appling HealthCare System
+<footer role="footer">
+	<section id="content-footer" class="container content">
+		<div class="row">
+			<div class="col-xs-4 first">
+				Appling HealthCare System
+			</div>
+			<div class="col-xs-4 second">
+				163 E. Tollison St Baxley, GA 31515
+			</div>
+			<div class="col-xs-4 third">
+				912-367-9841
+			</div>
 		</div>
-		<div class="col-xs-4 second">
-			163 E. Tollison St Baxley, GA 31515
-		</div>
-		<div class="col-xs-4 third">
-			912-367-9841
-		</div>
-	</div>
-</section>
- 
-<section id="logo-footer" class="content">
-	<img src="<?php echo $this->url("/assets/images/app_logo_125x80.jpg"); ?>" width="125" height="80" alt="Appling HealthCare Logo"/>
-</section>
-
-<section id="nav-footer" class="container content">
-	<p class="nav-menu">
-		<a href="<?php echo $this->urlAlias("home"); ?>">Home</a> | 
-		<a href="<?php echo $this->urlAlias("patient-visitor-services"); ?>">Patient &amp; Visitors</a> | 
-		<a href="<?php echo $this->urlAlias("physician-services-directory"); ?>">Pysicians &amp; Services</a> | 
-		<a href="<?php echo $this->urlAlias('coming-soon'); ?>">Billing &amp; Insurance</a> | 
-		<a href="<?php echo $this->urlAlias("about"); ?>">About Appling</a> | 
-		<a href="<?php echo $this->urlAlias("jobs"); ?>">Job Opportunities</a> | 
-		<a href="<?php echo $this->urlAlias("contact"); ?>">Contact Appling</a> | 
-		<a href="<?php echo $this->urlAlias('site-map'); ?>">Site Map</a>
-	</p>
-	<p class="nav-copyright">Copyright &copy; <?php echo date("Y"); ?> Appling HealthCare System | Powered by <a href="http://www.azaleahealth.com" target="_blank">Azalea Health</a></p>
-</section>
-
-<section id="associations-footer" class="container content">
-	<p class="associations"><img src="<?php echo $this->url("/assets/images/associations.jpg"); ?>" /></p>
-</section>
+	</section>
+	 
+	<section id="logo-footer" class="content">
+		<img src="<?php echo $this->url("/assets/images/app_logo_125x80.jpg"); ?>" width="125" height="80" alt="Appling HealthCare Logo"/>
+	</section>
+	
+	<nav id="nav-footer" class="container content" role="navigation">
+		<p class="nav-menu">
+			<a href="<?php echo $this->urlAlias("home"); ?>">Home</a> | 
+			<a href="<?php echo $this->urlAlias("patient-visitor-services"); ?>">Patient &amp; Visitors</a> | 
+			<a href="<?php echo $this->urlAlias("physician-services-directory"); ?>">Pysicians &amp; Services</a> | 
+			<a href="<?php echo $this->urlAlias('coming-soon'); ?>">Billing &amp; Insurance</a> | 
+			<a href="<?php echo $this->urlAlias("about"); ?>">About Appling</a> | 
+			<a href="<?php echo $this->urlAlias("jobs"); ?>">Job Opportunities</a> | 
+			<a href="<?php echo $this->urlAlias("contact"); ?>">Contact Appling</a> | 
+			<a href="<?php echo $this->urlAlias('site-map'); ?>">Site Map</a>
+		</p>
+		<p class="nav-copyright">Copyright &copy; <?php echo date("Y"); ?> Appling HealthCare System | Powered by <a href="http://www.azaleahealth.com" target="_blank">Azalea Health</a></p>
+	</nav>
+	
+	<section id="associations-footer" class="container content">
+		<p class="associations"><img src="<?php echo $this->url("/assets/images/associations.jpg"); ?>" alt="DNV Accredited Hospital. Member of Home Town Health" /></p>
+	</section>
+</footer>
