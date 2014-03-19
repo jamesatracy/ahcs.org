4a21708e84da458f65542cdd6ce958cc109dd163
James Tracy <james.a.tracy@gmail.com>
2014-03-19 16:51:00 -0700

    A11y updates

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -1,4 +1,4 @@
-<header id="main-page-header">
+<header id="main-page-header" role="banner">
 	<div class="container">
 		<div class="header-inner">
 			<div class="pull-left">
@@ -9,11 +9,11 @@
 			<div class="pull-right">
 				<div class="header-icons">
 					<p class="icons">
-						<a href="https://srvahcsptweb.myahcs.org" title="Hospital Patient Portal" target="_blank"><img class="hospital-portal" src="<?php echo $this->url("/assets/images/hospital-portal-icon.jpg"); ?>" width="40" height="56" alt="Hospital Patient Portal"/></a>
+						<a href="https://srvahcsptweb.myahcs.org" title="Hospital Patient Portal" target="_blank"><img class="hospital-portal" src="<?php echo $this->url("/assets/images/hospital-portal-icon.jpg"); ?>" width="40" height="56" alt="Hospital Patient Portal Link"/></a>
 						<a href="https://srvahcsptweb.myahcs.org" target="_blank"><small>Hospital Portal</small></a>
 					</p>
 					<p class="icons">
-						<a href="<?php echo $this->urlAlias('coming-soon'); ?>" title="Clinic Patient Portal -- Coming Soon!"><img class="clinic-portal" src="<?php echo $this->url("/assets/images/patient-portal-icon.jpg"); ?>" width="53" height="56" alt="Clinic Patient Portal"/></a>
+						<a href="<?php echo $this->urlAlias('coming-soon'); ?>" title="Clinic Patient Portal -- Coming Soon!"><img class="clinic-portal" src="<?php echo $this->url("/assets/images/patient-portal-icon.jpg"); ?>" width="53" height="56" alt="Clinic Patient Portal Link"/></a>
 						<a href="<?php echo $this->urlAlias('coming-soon'); ?>"><small>Clinic Portal</small></a>
 					</p>
 				</div>
