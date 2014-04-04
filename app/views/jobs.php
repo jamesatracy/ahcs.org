5318f02be24fd41bf9fad0dcfcb5dc138c6f4456
James Tracy <james.a.tracy@gmail.com>
2014-04-04 09:28:35 -0700

    Update jobs.php

--- app/views/jobs.php
+++ app/views/jobs.php
@@ -14,35 +14,16 @@ $this->pageHeader = "Job Opportunities";
 <p><img src="<?php echo $this->url('/assets/images/job-opportunities.jpg'); ?>" width="625" height="375" alt="Image of Appling Healthcare staff"/></p>
 
 <h3>Open Positions</h3>
-<p><strong>Purchasing –(Stock Clerk/Receptionist) – (Temporary- Fulltime 4-6 weeks only)</strong></p>
+<p><strong>Dialysis – (RN) – PRN</strong></p>
 <p>QUALIFICATIONS:</p>
 <ol>
- <li>Must be able to lift up to 60 lbs.</li>
- <li>Must maneuver heavy loads of supplies daily</li>
- <li>Physical activity such as bending, lifting, walking and standing for long periods of time</li>
- <li>Able to multi-task with good organizational skills</li>
- <li>Good communication and phone skills</li>
- <li>Ability to work in high pace environment</li>
- <li>Computer experience (Meditech preferred)</li>
+ <li>Must have current Georgia RN license</li>
+ <li>Current CPR certification</li>
+ <li>ACLS & PALS certified</li>
+ <li>Experience required</li>
 </ol>
 
-<p><strong>Hospital – (Auxiliary & Foundation Coordinator ) Part-time</strong></p>
-<p>Requirements: Must plan, prepare, organize & carry out activities of the Auxiliary, Volunteers and AHCS foundation, including community fundraising goals and events.</p>
-<p>QUALIFICATIONS:</p>
-<ol>
- <li>Past or present affiliation with civil or local organizations preferred</li>
- <li>Sales oriented</li>
- <li>Prior fundraising experience</li>
- <li>Basic computer skills</li>
- <li>Strong organizational skills</li>
- <li>Be a team leader</li>
- <li>Must be detail oriented</li>
- <li>Excellent communication skills</li>
-</ol>
-
-<p><strong>ER – (RN) – Fulltime - Flexible hours<br/>
-Med Surg - (RN) (1) - Fulltime (7a-7p)<br/>
-ICU – (RN) – Fulltime (7a-7p)</strong></p>
+<p><strong>Med Surg - (RN’s) (2) - Fulltime (7a-7p) & (7p-7a)</strong></p>
 <p>QUALIFICATIONS:</p>
 <ol>
  <li>Must have current Georgia license</li>
