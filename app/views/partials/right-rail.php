c229f19bf8c8613d72be0815fe3e55944d1ff31b
James Tracy <james.a.tracy@gmail.com>
2014-07-12 15:52:43 -0700

    Pull news feed from Wordpress posts.

--- app/views/partials/right-rail.php
+++ app/views/partials/right-rail.php
@@ -1,9 +1,21 @@
 <h3>Appling News</h3>
-<p><strong>Geriatric Psychiatric Beds Added</strong> <em>12/27/2013</em></p>
-<p>Appling HealthCare System was granted by certificate of need from the Georgia Department of Community Health, 15 additional Geriatric Psychiatric beds on December 27th, 2013.</p>
+<?php 
+global $wpdb;
+$results = $wpdb->get_results("SELECT * FROM wp_posts WHERE post_type = 'post' AND post_status = 'publish' ORDER BY post_date DESC LIMIT 5", OBJECT );
 
-<p><strong>Appling Healthcare ISO 9001 certified</strong> <em>12/27/2013</em></p>
-<p>Appling HealthCare System is one of two hospitals in the state of Georgia that is ISO 9001 certified.</p>
-
-<p><strong>Hometown Health Hospital of the Year!</strong></a> <em>11/14/2013</em></p>
-<p>Appling Healthcare System was awarded the 2013 hospital of the year award by Hometown Health at the fall conference at Callaway Gardens on November 14th, 2013. </p>
+foreach($results as $post) {
+?>
+  <article>
+    <header>
+      <p><strong><?php echo $post->post_title; ?></strong> <em><?php echo date("m/d/Y", strtotime($post->post_date)); ?></em></p>
+      <?php
+      $content = $post->post_content;
+      if(strlen($content) > 500) {
+        $content = substr($content, 500) . "...";
+      }
+      ?>
+      <p><?php echo $content; ?></p>
+  </article>
+<?php
+}
+?>
